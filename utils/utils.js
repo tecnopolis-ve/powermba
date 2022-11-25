f747eafe19d8d0a559a2e820819c12e2d67667fd
Manuel <manuel_se@live.com>
2022-11-25 11:48:39 +0100

    feat utils for generating password and random numbers

---
+++ utils/utils.js
@@ -0,0 +1,15 @@
+const generateNumber = () => {
+    return Math.floor(100000 + Math.random() * 900000);
+};
+
+const generateRandPassword = (length = 14) => {
+    const chars = 'abcdefghijklmnoprqstuvwxyz0123456789@#$%&_?-';
+    const charList = chars.split('');
+    const shuffledList = charList.sort((a, b) => 0.5 - Math.random());
+    return shuffledList.slice(0, length - 1).join('');
+}
+
+module.exports = {
+    generateNumber,
+    generateRandPassword,
+};
