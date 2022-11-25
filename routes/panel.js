5f80afcbd79d2a2ddb5b1e95396bb0d5a4b1775e
Manuel <manuel_se@live.com>
2022-11-25 17:39:18 +0100

    add panel routing for transactions and connections services

---
+++ routes/panel.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { checkAuth } = require("../middlewares/checkAuth");
+const connection = require('../controllers/connection');
+const transaction = require('../controllers/transaction');
+
+const router = express.Router();
+
+router.use(checkAuth);
+
+router.get("/connections", connection.list);
+router.get("/connection/:id", connection.get);
+router.post("/transaction", transaction.create);
+
+module.exports = router;
