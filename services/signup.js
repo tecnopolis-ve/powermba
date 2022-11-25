1a9f1d6e310dbbd7799027dfe37a334095ddc09d
Manuel <manuel_se@live.com>
2022-11-25 11:49:14 +0100

    feat new signup service

--- services/signup.js
+++ services/signup.js
@@ -1,11 +1,28 @@
 const bcrypt = require("bcrypt");
+const { generateNumber, generateRandPassword } = require("../utils/utils");
+const User = require("../models/User");
 
 async function signup(payload) {
     try {
+        const accountNumber = generateNumber();
+        const password = generateRandPassword(14);
+
+        const salt = await bcrypt.genSalt(10);
+        const saltedPassword = await bcrypt.hash(password, salt);
+
+        const user = new User({
+            name: payload.name,
+            lastName: payload.lastName,
+            dob: payload.dob,
+            password: saltedPassword,
+            accountNumber,
+        });
+        const savedUser = await user.save();
         return {
             status: 200,
             body: {
                 message: `User created!`,
+                data: { ...savedUser.toObject(), password },
             },
         };
     } catch (e) {
