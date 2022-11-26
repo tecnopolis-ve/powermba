4929496dbba19bbb14643690809c9bf5ab90f11b
Manuel <manuel_se@live.com>
2022-11-26 20:57:53 +0100

    add new helpers to calculate fees

--- services/balance.js
+++ services/balance.js
@@ -1,12 +1,23 @@
 const User = require("../models/User");
 
+function calculateFees(amount) {
+    let percentage = amount <= 1000 ? 1 : 0.5;
+    return parseFloat(((amount * percentage) / 100).toFixed(2));
+}
+
+function calculateAmountFees(amount, signed = true) {
+    const fees = calculateFees(amount);
+    return (signed ? -1 : 1) * (amount + fees);
+}
+
 async function checkBalance(userId, amount) {
     try {
         const user = await User.findById(userId);
         if (!user) {
             throw new Error(`Unable to process, user not found.`);
         }
-        return user.balance >= amount;
+        const fees = calculateFees(amount);
+        return user.balance >= amount + fees;
     } catch (e) {
         console.log(e);
         return false;
@@ -19,7 +30,7 @@ async function updateBalance(userId, amount) {
         if (!user) {
             throw new Error(`Unable to process, user not found.`);
         }
-        user.balance = user.balance + amount;
+        user.balance = parseFloat((user.balance + amount).toFixed(2));
         const result = await user.save();
         return result;
     } catch (e) {
@@ -31,4 +42,6 @@ async function updateBalance(userId, amount) {
 module.exports = {
     checkBalance,
     updateBalance,
+    calculateFees,
+    calculateAmountFees,
 };
