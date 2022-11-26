b4d7e7f074272242201f5548b79ae8d68540f19f
Manuel <manuel_se@live.com>
2022-11-26 17:22:22 +0100

    add balance field in User model

--- models/User.js
+++ models/User.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+        balance: {
+            type: Number,
+            required: true,
+        },
     },
     { timestamps: true }
 );
